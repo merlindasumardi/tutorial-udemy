78f0fa4a57e3b67b998cffc1c286554168f273a5
Merlinda Sumardi <merlinda.sumardi@gmail.com>
2018-02-04 23:28:58 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { SuccessComponent } from './success/success.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
